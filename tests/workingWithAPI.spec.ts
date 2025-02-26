78d4326e0513ff09b97e15178ae02c82afb7bd1d
germange <german.geton@gmail.com>
2025-02-26 13:35:53 +0200

    Mocking API test

---
+++ tests/workingWithAPI.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json'
+
+test.beforeEach( async ({page}) => {
+  await page.route('https://conduit-api.bondaracademy.com/api/tags', async route =>{
+    await route.fulfill({
+      body: JSON.stringify(tags)
+    })
+  })
+
+  await page.goto('https://conduit.bondaracademy.com/')
+  await page.waitForTimeout(1000)
+})
+
+test('has title', async ({ page }) => {
+  await expect(page.locator('.navbar-brand')).toHaveText('conduit')
+});
+
