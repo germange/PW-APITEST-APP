be8a01016150e9ceff0de3775deb8ef601011a3f
germange <german.geton@gmail.com>
2025-02-26 13:55:29 +0200

    Updating API responce

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -8,11 +8,25 @@ test.beforeEach( async ({page}) => {
     })
   })
 
+  await page.route('*/**/api/articles*', async route =>{
+    const response = await route.fetch()
+    const responseBody = await response.json()
+    responseBody.articles[0].title = "This is a test title"
+    responseBody.articles[0].description = "This is a test description"
+
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    })
+
+  })
+
   await page.goto('https://conduit.bondaracademy.com/')
   await page.waitForTimeout(1000)
 })
 
 test('has title', async ({ page }) => {
   await expect(page.locator('.navbar-brand')).toHaveText('conduit')
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title')
+  await expect(page.locator('app-article-list p').first()).toContainText('This is a test description')
 });
 
